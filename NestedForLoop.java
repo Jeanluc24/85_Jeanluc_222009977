4c1367f223617cff3dc98330d3d661f0e96faacd
Jeanluc24 <125446319+Jeanluc24@users.noreply.github.com>
2023-11-04 09:04:33 +0200

    Update NestedForLoop.java

--- NestedForLoop.java
+++ NestedForLoop.java
@@ -1,12 +1,20 @@
 class NestedForLoop {
     public static void main(String[] args) {
+        // Initialize a variable 'num' with the value 1.
         int num = 1;
 
+        // Outer loop: Iterate three times to create rows.
         for (int i = 1; i <= 3; i++) {
+            // Inner loop: Iterate three times to create columns.
             for (int j = 1; j <= 3; j++) {
+                // Print the current value of 'num' followed by a space.
                 System.out.print(num + " ");
+                
+                // Increment 'num' to the next value.
                 num++;
             }
+            
+            // Move to a new line after each row.
             System.out.println();
         }
     }
