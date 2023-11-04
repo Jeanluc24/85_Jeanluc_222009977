2931f17bc0278a3c141b436dcb6a5567215195bd
Jeanluc24 <125446319+Jeanluc24@users.noreply.github.com>
2023-11-04 09:16:25 +0200

    Update and rename Operexample7.java to RightShiftOperator.java

---
+++ RightShiftOperator.java
@@ -0,0 +1,19 @@
+class RightShiftOperator {
+    public static void main(String args[]) {
+        // Right Shift Operator (>>) shifts the bits of a number to the right.
+
+        // 10 in binary: 1010
+        // Shifting 2 bits to the right: 10 (in decimal, it's 2)
+        System.out.println(10 >> 2);  // Prints 2
+
+        // 20 in binary: 10100
+        // Shifting 2 bits to the right: 101 (in decimal, it's 5)
+        System.out.println(20 >> 2);  // Prints 5
+
+        // 20 in binary: 10100
+        // Shifting 3 bits to the right: 10 (in decimal, it's 2)
+        System.out.println(20 >> 3);  // Prints 2
+    }
+}
+
+
