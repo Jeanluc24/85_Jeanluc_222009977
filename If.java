66a7d99db7b941c017fae4c18e8ee31e80d726d6
Jeanluc24 <125446319+Jeanluc24@users.noreply.github.com>
2023-11-04 09:01:22 +0200

    Update If.java

--- If.java
+++ If.java
@@ -1,14 +1,16 @@
 class If {
-  public static void main(String args[]) {
-    int a = 10;
-    int b = 15;
-    if (b > a) {
-      System.out.println("b is greater than a");
-    }
+    public static void main(String args[]) {
+        // Declare and initialize two integer variables 'a' and 'b'.
+        int a = 10;
+        int b = 15;
 
-    else {
-      System.out.println("a is greater than b");
+        // Check if 'b' is greater than 'a'.
+        if (b > a) {
+            // If the condition is true, print that 'b' is greater than 'a'.
+            System.out.println("b is greater than a");
+        } else {
+            // If the condition is false, print that 'a' is greater than 'b'.
+            System.out.println("a is greater than b");
+        }
     }
-
-  }
 }
